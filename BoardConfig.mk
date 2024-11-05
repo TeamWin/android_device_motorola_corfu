b20a880136b0cd924dcf3edc513676eeb31d657e
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-05 16:02:50 -0500

    Corfu: NO FASTBOOT BOOT
    *unable to reboot to system etc. When this flag is set to
     true, twrpfastboot=1 isn't added into the kernel cmdline and therefore

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,7 @@ TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_HAS_NO_RECOVERY_PARTITION := true
 TW_EXCLUDE_APEX := true
+TW_NO_FASTBOOT_BOOT := true
 
 #Brightness
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
