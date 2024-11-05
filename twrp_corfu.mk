1123783b714c7dcc03e83d884e1d5c881725dbc9
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-05 20:38:00 +0200

    Corfu: initial TWRP tree

---
+++ twrp_corfu.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from corfu device
+$(call inherit-product, device/motorola/corfu/device.mk)
+
+PRODUCT_DEVICE := corfu
+PRODUCT_NAME := twrp_corfu
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g41
+PRODUCT_MANUFACTURER := motorola
+
+
